771b5f2574abafbb3a868ac443797038d169a832
colehill <162524929+colehill@users.noreply.github.com>
2024-03-20 10:55:41 +0800

    Add files via upload

---
+++ jump.js
@@ -0,0 +1,10 @@
+function fmk() {
+    console.log("fmk function has been called.");
+    // 这里可以添加更多的逻辑
+}
+
+// 定义 showline 函数
+function showline() {
+    fmk(); // 调用 fmk 函数
+    window.open('https://open.kakao.com/o/gRdXHxeg'); // 打开新的链接
+}
